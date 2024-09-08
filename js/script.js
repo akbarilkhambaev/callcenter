afd21a4a20deda2e3f7cef3605ddd2a44ea5dd8f
akbarilkhambaev <99265249+akbarilkhambaev@users.noreply.github.com>
2024-09-08 12:12:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,64 +1,7 @@
-var images = document.querySelectorAll('.sliding');
-document.querySelector('.next').onclick = next;
-document.querySelector('.prev').onclick = prev;
-var counter = 0;
-function next(){
-    images[counter].classList.remove('active');
-    li[counter].classList.remove('active-li');
-    counter++;
-    if(counter == images.length){
-        counter = 0;
-    }
-    images[counter].classList.add('active');
-    li[counter].classList.add('active-li');
-
-}
-
-function prev(){
-    images[counter].classList.remove('active');
-    li[counter].classList.remove('active-li');
-    counter--;
-    if(counter < 0){
-        counter = --images.length;
-    }
-    images[counter].classList.add('active');
-    li[counter].classList.add('active-li');
-
-}
-
-var timer;
-function auto(){
-    timer = setInterval(next, 3000);
-}
-auto();
-
-var li = document.querySelectorAll('.indicators li');
-
-for(var i = 0; i<li.length; i++){
-    li[i].onclick = function () {
-        for(var j = 0; j < li.length; j++){
-            li[j].classList.remove('active-li');
-            images[j].classList.remove('active');    
-        }
-        var current = this.getAttribute('data-slider');
-        images[current].classList.add('active');
-        this.classList.add('active-li');
-        counter = current;   
-    }
-}
-
-var slider = document.querySelector('.slide-show');
-slider.onmouseover = function(){
-    clearInterval(timer);
-}
-slider.onmouseout = function(){
-    auto();
-}
-
-
 var containerheader = document.querySelector('.container-header');
 var body = document.querySelector('body');
-var sliders = document.querySelector('.slide-show');
+var cntMenu = document.querySelector('.content-menu');
+var forSlide = document.querySelector('.for-sld');
 
 function updateLayout() {
     var bodyWidth = document.body.clientWidth; // Обновляем ширину
@@ -66,13 +9,22 @@ function updateLayout() {
     if (bodyWidth < 768) {
         containerheader.classList.remove('container');
         containerheader.classList.add('container-fluid');
-        slider.classList.remove('container');
-        slider.classList.add('container-fluid');
+        cntMenu.classList.remove('container');
+        cntMenu.classList.add('container-fluid');
     } else {
         containerheader.classList.remove('container-fluid');
         containerheader.classList.add('container');
-        slider.classList.add('container');
-        slider.classList.remove('container-fluid');
+        cntMenu.classList.add('container');
+        cntMenu.classList.remove('container-fluid');
+    }
+
+    if(bodyWidth < 1199){
+        forSlide.classList.remove('container');
+        forSlide.classList.add('container-fluid');
+    }
+    else {
+        forSlide.classList.add('container');
+        forSlide.classList.remove('container-fluid');
     }
 
     console.log(bodyWidth);
@@ -81,3 +33,53 @@ function updateLayout() {
 updateLayout();
 
 window.addEventListener('resize', updateLayout);
+
+
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+let currentSlide = 0;
+
+document.getElementById('next').addEventListener('click', () => {
+    changeSlide(currentSlide + 1);
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+    changeSlide(currentSlide - 1);
+});
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        changeSlide(index);
+    });
+});
+
+function changeSlide(index) {
+    if (index >= slides.length) {
+        index = 0;
+    } else if (index < 0) {
+        index = slides.length - 1;
+    }
+    
+    slides[currentSlide].classList.remove('active');
+    dots[currentSlide].classList.remove('active');
+    
+    currentSlide = index;
+    
+    slides[currentSlide].classList.add('active');
+    dots[currentSlide].classList.add('active');
+    
+    document.querySelector('.slides').style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+var toggler = document.querySelector('.toggler');
+var navBar = document.querySelector('.nav-bar');
+
+addEventListener('click', function(){
+    toggler.classList.toggle('change');
+    
+});
+
+addEventListener('click', function(){
+    navBar.classList.toggle('hide-show');
+});
+
