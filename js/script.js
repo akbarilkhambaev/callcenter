a98427b63569030af485dc4e35c3986ef7232611
akbarilkhambaev <99265249+akbarilkhambaev@users.noreply.github.com>
2024-09-08 15:40:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -34,23 +34,44 @@ updateLayout();
 
 window.addEventListener('resize', updateLayout);
 
-
 const slides = document.querySelectorAll('.slide');
 const dots = document.querySelectorAll('.dot');
 let currentSlide = 0;
+let autoSlideInterval;
+
+// Автоматическая смена слайдов каждые 5 секунд
+autoSlideInterval = setInterval(() => {
+    changeSlide(currentSlide + 1);
+}, 3000);
 
 document.getElementById('next').addEventListener('click', () => {
+    clearInterval(autoSlideInterval); // Останавливаем автоматическую смену при ручной навигации
     changeSlide(currentSlide + 1);
+    resetAutoSlide(); // Перезапускаем автоматическую смену слайдов
 });
 
 document.getElementById('prev').addEventListener('click', () => {
+    clearInterval(autoSlideInterval);
     changeSlide(currentSlide - 1);
+    resetAutoSlide();
 });
 
 dots.forEach((dot, index) => {
     dot.addEventListener('click', () => {
+        clearInterval(autoSlideInterval);
         changeSlide(index);
+        resetAutoSlide();
     });
+});
+
+// Остановка автоматической смены при наведении мыши
+document.querySelector('.slides').addEventListener('mouseover', () => {
+    clearInterval(autoSlideInterval);
+});
+
+// Возобновление автоматической смены при убирании мыши
+document.querySelector('.slides').addEventListener('mouseout', () => {
+    resetAutoSlide();
 });
 
 function changeSlide(index) {
@@ -71,6 +92,14 @@ function changeSlide(index) {
     document.querySelector('.slides').style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 
+// Перезапуск автоматической смены слайдов
+function resetAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+        changeSlide(currentSlide + 1);
+    }, 5000);
+}
+
+
 var toggler = document.querySelector('.toggler');
 var navBar = document.querySelector('.nav-bar');
 
@@ -78,4 +107,24 @@ toggler.addEventListener('click', function(){
         toggler.classList.toggle('change');
         navBar.classList.toggle('hide-show');
 });
+
+
+var form = document.querySelector('form');
+var input = document.querySelectorAll('input');
+
+form.addEventListener('submit', function(e){
+    for(var i = 0; i < input.length; i++){
+        if(input[i].value == ''){
+            input[i].classList.add('error');
+            e.preventDefault();
+        }
+    }
+})
+
+for(var i = 0; i < input.length; i++){
+    input[i].addEventListener('input', function (){
+        this.classList.remove('error');
+
+    })
+}
 
