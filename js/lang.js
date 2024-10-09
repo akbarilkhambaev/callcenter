39686b13c1d3284ed59149ef3098aa11a8742fcc
akbarilkhambaev <99265249+akbarilkhambaev@users.noreply.github.com>
2024-10-09 13:30:29 +0500

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,280 @@
+const langArray = {
+  call: {
+    uz: "Biz bilan bog'laning ",
+    ru: 'Заказать обратный звонок',
+    en: 'Contact us',
+  },
+  request: {
+    uz: 'ARIZA QOLDIRISH!',
+    ru: 'ОСТАВИТЬ ЗАЯВКУ!',
+    en: 'SEND A REQUEST!',
+  },
+  main: {
+    uz: 'Bosh sahifa',
+    ru: 'Главная',
+    en: 'Home',
+  },
+  main2: {
+    uz: 'Bosh sahifa',
+    ru: 'Главная',
+    en: 'Home',
+  },
+  goods: {
+    uz: 'Maxsulotlar',
+    ru: 'Продукия',
+    en: 'Goods',
+  },
+  goods2: {
+    uz: 'Maxsulotlar',
+    ru: 'Продукия',
+    en: 'Goods',
+  },
+  galery: {
+    uz: 'Galereya',
+    ru: 'Галерея',
+    en: 'Galery',
+  },
+  galery2: {
+    uz: 'Galereya',
+    ru: 'Галерея',
+    en: 'Galery',
+  },
+  about: {
+    uz: 'Biz haqimizda',
+    ru: 'О нас',
+    en: 'About us',
+  },
+  about2: {
+    uz: 'Biz haqimizda',
+    ru: 'О нас',
+    en: 'About us',
+  },
+  contacts: {
+    uz: 'Kontaktlarimiz',
+    ru: 'Контакты',
+    en: 'Contacts',
+  },
+  contacts2: {
+    uz: 'Kontaktlarimiz',
+    ru: 'Контакты',
+    en: 'Contacts',
+  },
+  systems: {
+    uz: 'GILYATINA SISTEMALARI',
+    ru: 'ГИЛЬОТИННЫЕ СИСТЕМЫ',
+    en: 'GUILLOTINE SYSTEMS',
+  },
+  systems_3: {
+    uz: 'Pergola',
+    ru: 'Пергола',
+    en: 'Pergolas',
+  },
+  systems_4: {
+    uz: 'Gilyatina',
+    ru: 'Гильятина',
+    en: 'Guillotine',
+  },
+  adress: {
+    uz: `Bobur ko'chasi 1 uy`,
+    ru: 'Улица Бобура дом №1',
+    en: 'No. 1 Bobura Street',
+  },
+  work_time: {
+    uz: `Du. - Sh.: 9:00 dan 18:00 gacha.`,
+    ru: 'Пн. - Сб.: 9:00 до 18:00',
+    en: 'Mon. - Sat.: 9:00 to 18:00',
+  },
+  perilla: {
+    uz: 'Shisha perillalar',
+    ru: 'Стеклянные периллы',
+    en: 'Glass railings',
+  },
+  autodoor: {
+    uz: 'Sensor eshiklar',
+    ru: 'Сенсорные двери',
+    en: 'Sensor doors',
+  },
+  description: {
+    uz: "AKFA Innovation kompaniyasining gilyotinli oynasi - bar, restoran va mehmonxonalarning teraslari va verandalari uchun optimal echim. Vertikal ravishda ko'tarilgan derazalar mehmonlarni kuchli shamollardan, jazirama quyoshdan, yomg'irdan va changdan himoya qiladi, shuningdek mehmonlar xavfsizligi va mol-mulk xavfsizligini ta'minlashga yordam beradi.",
+    ru: 'Оптимальное решение для террас и веранд баров, ресторанов и гостиниц. Вертикально поднимающиеся окна защитят гостей от сильного ветра, палящего солнца, дождя и пыли, а так же помогут обеспечить безопасность гостей и сохранность имущества.',
+    en: 'Guillotine glazing from AKFA Innovation is the optimal solution for terraces and verandas of bars, restaurants and hotels. Vertically rising windows will protect guests from strong winds, scorching sun, rain and dust, as well as help to ensure the safety of guests and the safety of property.',
+  },
+  lmore: {
+    uz: `» Ko'proq bilish «`,
+    ru: '» Узнать больше «',
+    en: '» See more «',
+  },
+  systems2: {
+    uz: 'PERGOLA',
+    ru: 'ПЕРГОЛА',
+    en: 'PERGOLAS',
+  },
+  description2: {
+    uz: "Pergola tizimi - bu alyuminiy soyabon bo'lib, u soya beradi va landshaft dizaynining o'ziga xos elementini tashkil qiladi. Ushbu tizim teraslar, kafelarda yozgi maydonlar, xususiy uylarda gazebolar qurilishida qo'llaniladi. Pergolalar sizni hududni chegaralashga imkon beradi, shu bilan uni bir nechta joyga ajratadi.",
+    ru: 'Тентовые перголы — один из наиболее эффективных и практичных видов зонирования открытого пространства. С помощью перголы можно обустроить зону отдыха во дворе загородного дома или на общественной территории. Тентовый навес позволяет отрегулировать уровень освещения и защититься от солнечных лучей и дождей.',
+    en: 'The pergola system is an aluminum canopy that provides shade to the area and forms an original element of landscape design. This system is used in the construction of terraces, summer areas in cafes, gazebos in private houses. Pergolas allow you to delimit the area, thereby dividing it into several locations',
+  },
+  systems3: {
+    uz: 'SLIDE MASTER',
+    ru: 'SLIDE MASTER',
+    en: 'SLIDE MASTER',
+  },
+  description3: {
+    uz: "Issiqlik izolyatsiyasiga ega SlideMaster surilma tizimi — balkon uchun oyna tizimlarining yangi standarti. SlideMaster surilma tizimi issiqlik izolyatsiyasi, qulaylik va estetika nuqtai nazaridan yangilandi. Sizning balkonlaringiz, qishki bog'laringiz va restoranlaringizni qulay va shinam joyga aylanadi.",
+    ru: 'SlideMaster - Раздвижная система со стеклопакетом - повышает стандарты систем остекления балконов. Раздвижные системы были улучшены с точки зрения изоляции, комфорта и эстетики. Благодаря этому, превратят ваши балконы, зимние сады и рестораны в удивительно теплые помещения.',
+    en: 'SlideMaster - a thermally insulated sliding system - raises the standards of balcony glazing systems. Sliding systems have been updated in terms of thermal insulation, comfort and aesthetics and transform your balconies, winter gardens and restaurants into amazing living areas.',
+  },
+  whywe: {
+    uz: 'NEGA AYNAN BIZ!',
+    ru: 'ПОЧЕМУ ВЫБИРАЮТ ИМЕННО  НАС!',
+    en: 'WHY CHOOSE US!',
+  },
+  measuring: {
+    uz: "O'LCHOVLAR BEPUL",
+    ru: 'ЗАМЕРЫ БЕСПЛАТНО',
+    en: 'FREE MEASURMENTS',
+  },
+  guarantee: {
+    uz: '5 YILGA KAFOLAT',
+    ru: '5 ЛЕТ ГАРАНТИИ',
+    en: '5 YEARS WARRANTY',
+  },
+  expiration: {
+    uz: 'QISQA MUDDATLAR',
+    ru: 'КОРОТКИЕ СРОКИ',
+    en: 'SHORT TERMS',
+  },
+  standarts: {
+    uz: 'GOST STANDARTLARI',
+    ru: 'СТАНДАРТЫ ГОСТ',
+    en: 'GOST STANDARTS',
+  },
+  montage: {
+    uz: 'BEPUL MONTAJ',
+    ru: 'МОНТАЖ БЕСПЛАТНО',
+    en: 'FREE INSTALLATION',
+  },
+  out_projects: {
+    uz: 'Bizning loyihalarimiz',
+    ru: 'Наши проекты',
+    en: 'Our projects',
+  },
+  project1: {
+    uz: 'Loyiha 1 : Qishloq joyidagi terassa',
+    ru: 'Проект 1: Терраса в загородном доме',
+    en: 'Project 1: Terrace in a country house',
+  },
+  descrip_project1: {
+    uz: `<strong>Tavsif: </strong> ushbu loyihada biz BKG 40 surma eshik tizimimizdan foydalangan holda keng terassa yaratdik. Mijoz qulaylik va maxfiylikni saqlab, ichki va tashqi makonni birlashtirmoqchi edi. Natijada zamonaviy va funktsional terassa hosil bo'lib, oilalar uchun juda mos keladi.`,
+    ru: '<strong >Описание:</strong> В этом проекте мы создали просторную террасу с использованием нашей системы раздвижных дверей BKG 40. Клиент хотел объединить внутреннее и внешнее пространство, сохранив при этом уют и приватность. Результат — стильная и функциональная терраса, идеально подходящая для семейного отдыха.',
+    en: '<strong >Description:</strong> In this project, we have created a spacious terrace using our BKG 40 sliding door system. The client wanted to combine the internal and external space, while maintaining comfort and privacy. The result is a stylish and functional terrace, ideal for a family holiday.',
+  },
+  project2: {
+    uz: 'Loyiha 2: Dengiz manzarali restoran',
+    ru: 'Проект 2: Ресторан с видом на море',
+    en: 'Project 2: Restaurant with sea view',
+  },
+  descrip_project2: {
+    uz: "<strong>Tavsif: </strong> ushbu restoran uchun biz vertikal ravishda ko'tarilgan derazalarni o'rnatdik, ular mehmonlarni kuchli shamol va jazirama quyoshdan himoya qiladi, shu bilan birga dengizning go'zal manzarasini to'sib qo'ymaydi. Terassa tadbirlar va  uchrashuvlar uchun mashhur joyga aylandi.",
+    ru: '<strong>Описание:</strong> Для этого ресторана мы установили вертикально поднимающиеся окна, которые защищают гостей от сильного ветра и палящего солнца, не перекрывая при этом прекрасный вид на море. Терраса стала популярным местом для проведения мероприятий и частных встреч.',
+    en: '<strong>Description:</strong> For this restaurant, we have installed vertically rising windows that protect guests from strong winds and the scorching sun, while not blocking the beautiful view of the sea. The terrace has become a popular venue for events and private meetings.',
+  },
+  project3: {
+    uz: 'Loyiha-3: Tijorat binosi',
+    ru: 'Проект 3: Коммерческое здание',
+    en: 'Project 3: Commercial building',
+  },
+  descrip_project3: {
+    uz: "<strong>Tavsif: </strong> ushbu loyiha doirasida biz shahar markazidagi tijorat binosi uchun terasta ustida ishladik. Asosiy vazifa ish uchun ham, dam olish uchun ham ishlatilishi mumkin bo'lgan ko'p funktsiyali makonni yaratish edi. Bizning bo'linma tizimimiz tufayli teras osongina o'zgartiriladi va turli vazifalarga moslashadi.",
+    ru: '<strong>Описание:</strong> В рамках данного проекта мы работали над террасой для коммерческого здания в центре города. Основной задачей было создание многофункционального пространства, которое могло бы использоваться как для работы, так и для отдыха. Благодаря нашей системе перегородок, терраса легко трансформируется и адаптируется к различным задачам.',
+    en: '<strong>Description:</strong> As part of this project, we worked on a terrace for a commercial building in the city center. The main task was to create a multifunctional space that could be used for both work and leisure. Thanks to our partition system, the terrace can be easily transformed and adapted to various tasks.',
+  },
+  project4: {
+    uz: "loyiha-4: Xususiy bog'",
+    ru: 'Проект 4: Приватный сад',
+    en: 'Project 4: Private garden',
+  },
+  descrip_project4: {
+    uz: "<strong>Tavsif: </strong> ushbu loyihada biz tabiiy materiallardan maksimal darajada foydalanadigan yopiq terastani ishlab chiqdik. Mijoz ko'katlar bilan o'ralgan dam olish joyini yaratmoqchi edi. Natijada bog'ning umumiy dizayniga juda mos keladigan qulay teras.",
+    ru: '<strong>Описание:</strong> В этом проекте мы разработали закрытую террасу с максимальным использованием природных материалов. Клиент хотел создать пространство для релаксации, окруженное зеленью. В результате получилась уютная терраса, идеально вписывающаяся в общий дизайн сада.',
+    en: '<strong>Description:</strong> In this project, we have developed an enclosed terrace with maximum use of natural materials. The client wanted to create a relaxation space surrounded by greenery. The result is a cozy terrace that fits perfectly into the overall design of the garden.',
+  },
+  partners: {
+    uz: 'Bizning hamkorlarimiz',
+    ru: 'Наши партнеры',
+    en: 'Our partners',
+  },
+  feed_back: {
+    uz: 'Qayta aloqa!',
+    ru: 'Обратная связь!',
+    en: 'Feedback!',
+  },
+  submit: {
+    uz: 'Yuborish',
+    ru: 'Отправить',
+    en: 'Submit',
+  },
+  about_company: {
+    uz: 'Kompaniya haqida',
+    ru: 'О компании',
+    en: 'About company',
+  },
+  our_products: {
+    uz: 'Bizning maxsulotlar',
+    ru: 'Наши продукты',
+    en: 'Our products',
+  },
+  social: {
+    uz: 'Ijtimoiy tarmoqlar',
+    ru: 'Наши соц.сети',
+    en: 'Social networks',
+  },
+  contact_info: {
+    uz: `Bog'lanish`,
+    ru: 'Контактные данные',
+    en: 'Contant Info',
+  },
+};
+const select = document.querySelectorAll('.lang');
+const allLang = ['uz', 'ru', 'en'];
+
+select.forEach((el) => {
+  el.addEventListener('click', () => {
+    const lang = el.alt;
+    // Сохраняем выбранный язык в LocalStorage
+    localStorage.setItem('language', lang);
+    changeURLLanguage(lang);
+  });
+});
+
+function changeURLLanguage(lang) {
+  // Изменяем хеш URL на выбранный язык
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
+}
+
+function changeLanguage() {
+  // Получаем выбранный язык из LocalStorage
+  let lang = localStorage.getItem('language') || 'ru'; // По умолчанию 'ru'
+
+  if (!allLang.includes(lang)) {
+    lang = 'ru';
+  }
+
+  select.forEach((el) => {
+    el.alt === lang
+      ? el.classList.add('active')
+      : el.classList.remove('active');
+  });
+
+  // Обновляем контент страницы на выбранный язык
+  for (let key in langArray) {
+    let elem = document.querySelector('.lng-' + key);
+    if (elem) {
+      elem.innerHTML = langArray[key][lang];
+    }
+  }
+}
+
+// Вызываем при загрузке страницы
+changeLanguage();
--- js/script.js
+++ js/script.js
@@ -2,129 +2,28 @@ var containerheader = document.querySelector('.container-header');
 var body = document.querySelector('body');
 var cntMenu = document.querySelector('.content-menu');
 var forSlide = document.querySelector('.for-sld');
-
-function updateLayout() {
-    var bodyWidth = document.body.clientWidth; // Обновляем ширину
-
-    if (bodyWidth < 768) {
-        containerheader.classList.remove('container');
-        containerheader.classList.add('container-fluid');
-        cntMenu.classList.remove('container');
-        cntMenu.classList.add('container-fluid');
-    } else {
-        containerheader.classList.remove('container-fluid');
-        containerheader.classList.add('container');
-        cntMenu.classList.add('container');
-        cntMenu.classList.remove('container-fluid');
-    }
-
-    if(bodyWidth < 1199){
-        forSlide.classList.remove('container');
-        forSlide.classList.add('container-fluid');
-    }
-    else {
-        forSlide.classList.add('container');
-        forSlide.classList.remove('container-fluid');
-    }
-
-    console.log(bodyWidth);
-}
-
-updateLayout();
-
-window.addEventListener('resize', updateLayout);
-
-const slides = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.dot');
-let currentSlide = 0;
-let autoSlideInterval;
-
-// Автоматическая смена слайдов каждые 5 секунд
-autoSlideInterval = setInterval(() => {
-    changeSlide(currentSlide + 1);
-}, 3000);
-
-document.getElementById('next').addEventListener('click', () => {
-    clearInterval(autoSlideInterval); // Останавливаем автоматическую смену при ручной навигации
-    changeSlide(currentSlide + 1);
-    resetAutoSlide(); // Перезапускаем автоматическую смену слайдов
-});
-
-document.getElementById('prev').addEventListener('click', () => {
-    clearInterval(autoSlideInterval);
-    changeSlide(currentSlide - 1);
-    resetAutoSlide();
-});
-
-dots.forEach((dot, index) => {
-    dot.addEventListener('click', () => {
-        clearInterval(autoSlideInterval);
-        changeSlide(index);
-        resetAutoSlide();
-    });
-});
-
-// Остановка автоматической смены при наведении мыши
-document.querySelector('.slides').addEventListener('mouseover', () => {
-    clearInterval(autoSlideInterval);
-});
-
-// Возобновление автоматической смены при убирании мыши
-document.querySelector('.slides').addEventListener('mouseout', () => {
-    resetAutoSlide();
-});
-
-function changeSlide(index) {
-    if (index >= slides.length) {
-        index = 0;
-    } else if (index < 0) {
-        index = slides.length - 1;
-    }
-    
-    slides[currentSlide].classList.remove('active');
-    dots[currentSlide].classList.remove('active');
-    
-    currentSlide = index;
-    
-    slides[currentSlide].classList.add('active');
-    dots[currentSlide].classList.add('active');
-    
-    document.querySelector('.slides').style.transform = `translateX(-${currentSlide * 100}%)`;
-}
-
-// Перезапуск автоматической смены слайдов
-function resetAutoSlide() {
-    autoSlideInterval = setInterval(() => {
-        changeSlide(currentSlide + 1);
-    }, 5000);
-}
-
-
+var productSection = document.querySelector('.product-section');
 var toggler = document.querySelector('.toggler');
 var navBar = document.querySelector('.nav-bar');
+var toggleButton = document.querySelectorAll('.button_absolute');
 
-toggler.addEventListener('click', function(){
-        toggler.classList.toggle('change');
-        navBar.classList.toggle('hide-show');
+toggler.addEventListener('click', function () {
+  toggler.classList.toggle('change');
+  navBar.classList.toggle('hide-show');
 });
 
-
-var form = document.querySelector('form');
-var input = document.querySelectorAll('input');
-
-form.addEventListener('submit', function(e){
-    for(var i = 0; i < input.length; i++){
-        if(input[i].value == ''){
-            input[i].classList.add('error');
-            e.preventDefault();
-        }
+toggleButton.forEach(function (button, index) {
+  button.addEventListener('click', function () {
+    var description = document.querySelectorAll('.description')[index]; // Находим соответствующий description по индексу
+    var toggle = description.classList.toggle('description_show'); // Переключаем видимость
+    if (toggle) {
+      toggleButton[index].innerText = 'Меньше';
+    } else {
+      toggleButton[index].innerText = 'Узнать больше';
     }
-})
+  });
+});
+console.log(toggleButton[0].innerText);
 
-for(var i = 0; i < input.length; i++){
-    input[i].addEventListener('input', function (){
-        this.classList.remove('error');
-
-    })
-}
-
+window.addEventListener('resize', updateLayout);
+updateLayout();
